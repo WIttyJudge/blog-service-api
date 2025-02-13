52b6e3001cf3af0704550c50cbcc7b21f86bd8a8
Ruslan <kros8925@gmail.com>
2025-02-13 18:59:30 +0200

    feat: add response for healthz endpoint

--- internal/api/healthcheck.go
+++ internal/api/healthcheck.go
@@ -1,13 +1,12 @@
 package api
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func (a *API) healthzHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		resp := map[string]string{"status": "ok"}
-		_ = json.NewEncoder(w).Encode(resp)
+		a.successResponse(w, resp, http.StatusOK)
 	}
 }
